fc3890bdde0a45ba9d10041702d092e8e0e05a49
shravankumar279 <shravan.it@hotmail.com>
2021-12-26 15:36:45 +0530

    T002-Create-File

--- terraform/linux_env_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# install terraform
-cd /tmp 
-wget https://releases.hashicorp.com/terraform/1.1.2/terraform_1.1.2_linux_amd64.zip
-unzip terraform_1.1.2_linux_amd64.zip
-mv terraform /usr/local/bin
-terraform version
