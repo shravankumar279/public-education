c4e74608a57e5289d22d19f25110378b3dd52ce1
shravankumar279 <32362863+shravankumar279@users.noreply.github.com>
2021-12-26 13:28:49 +0530

    Update env_setup.sh

--- terraform/env_setup.sh
+++ terraform/env_setup.sh
@@ -3,6 +3,6 @@
 # install terraform
 cd /tmp 
 wget https://releases.hashicorp.com/terraform/1.1.2/terraform_1.1.2_ubuntu_amd64.zip
-unzip terraform_1.1.2_windows_amd64.zip
+unzip terraform_1.1.2_ubuntu_amd64.zip
 mv terraform /usr/local/bin
 terraform version
