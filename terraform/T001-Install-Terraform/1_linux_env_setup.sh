c3bd0f0b0d44aa3a3923adc08e29a08156f46187
shravankumar279 <shravan.it@hotmail.com>
2021-12-26 15:38:25 +0530

    T002-Create-File

---
+++ terraform/T001-Install-Terraform/1_linux_env_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# install terraform
+cd /tmp 
+wget https://releases.hashicorp.com/terraform/1.1.2/terraform_1.1.2_linux_amd64.zip
+unzip terraform_1.1.2_linux_amd64.zip
+mv terraform /usr/local/bin
+terraform version
---
+++ terraform/T002-Create-File/1-create-file.tf
@@ -0,0 +1,4 @@
+resource "local_file" "mypet" {
+  filename = "/root/T002-Create-File/mypet"
+  content  = "We love pets"
+}
