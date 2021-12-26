6a359a272ecd4fa7576ba9fed2418029a30d67a9
shravankumar279 <32362863+shravankumar279@users.noreply.github.com>
2021-12-26 13:16:31 +0530

    install terraform

--- terraform/env_setup.sh
+++ terraform/env_setup.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+# install terraform
+cd /tmp 
+wget https://releases.hashicorp.com/terraform/1.1.2/terraform_1.1.2_windows_amd64.zip
+unzip terraform_1.1.2_windows_amd64.zip
+mv terraform /usr/local/bin
+terraform version
